ec74737ec5d792aa47e04baed26c7a3d4fadbd13
KhromovNikita <Nikita.Khromov@onlyoffice.com>
2023-01-13 10:27:25 +0500

    Rework

--- main.js
+++ main.js
@@ -1,63 +1,48 @@
+const express = require('express');
+var bodyParser = require('body-parser')
 const needle = require('needle');
-const http = require('http');
-const { head } = require('needle');
+var cors = require('cors');
+const app = express();
+//app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(cors({
+	origin: '*'
+}));
+const PORT = process.env.PORT || 5000
 
-// server
-const hostname = '127.0.0.1';
-const port = 5000;
+app.get('/', (req, res) => {
+	res.send({ message: 'You should to use post request:)' });
+});
 
-const server = http.createServer((request, response) => {
-	let data = "";
-	let headers = {}
-
-	// копируем все заголовки запроса, кроме "host" и targetUrl
-	for (let key in request.headers) {
-		if (key !== "host" && key !== "target")
-			headers[key] = request.headers[key];
-	}
-	const options = {
+app.post('/', (req, res) => {
+	// Здесь будем создавать заметку.
+	res.statusCode = 200
+	res.setHeader('Content-Type', 'text/plain')
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	let targetUrl = "https://api.clarivate.com/apis/wos-starter/v1/documents";
+	let method = 'GET';
+	let data = {};
+	let options = {
 		headers: {
-			"content-type": 'application/json'
+			"X-ApiKey": `d33fb239c06530922e8d200606f741c980e3e286`
 		}
 	};
 
-	// загрузка данных
-	request.on("data", chunk => {
-		data += chunk;
+	needle(method, targetUrl, data, options)
+		.then((target_response) => {
+			console.log(`Status: ${target_response.statusCode}`);
+			console.log('Body: ', target_response.body);
+			res.statusCode = target_response.statusCode;
+			res.setHeader('Access-Control-Allow-Origin', '*');
+			res.statusText = JSON.stringify(target_response.body);
+			res.setHeader('Content-Type', 'text/plain');
+			res.end(JSON.stringify(target_response.body));
+		}).catch((err) => {
+		response.end("Error of request");
+		console.error(err);
 	});
 
-	// в конце загрузки данных посылаем запрос по targetUrl
-	request.on("end", () => {
-		try {
-			data = JSON.parse(data);
-			options["headers"]["Authorization"] = data["Authorization"];
-			let method = data["method"];
-			let targetUrl = "https://api.zoom.us/v2/users/" + data["endPoint"];
+});
 
-			needle(method, targetUrl, data, options)
-				.then((target_response) => {
-					console.log(`Status: ${target_response.statusCode}`);
-					console.log('Body: ', target_response.body);
-					response.statusCode = target_response.statusCode;
-					response.setHeader('Access-Control-Allow-Origin', '*');
-					response.statusText = JSON.stringify(target_response.body);
-					response.setHeader('Content-Type', 'text/plain');
-					response.end(JSON.stringify(target_response.body));
-				}).catch((err) => {
-					response.end("Error of request");
-					console.error(err);
-			});
-		}
-		catch (err) {
-			console.log(err);
-			response.end(err);
-		}
-		
-	});
-})
-server.listen(port, hostname, () => {
-	console.log(`Server running at http://${hostname}:${port}/`)
-})
-
-
-
+app.listen(PORT, () => {
+	console.log('We are live on ' + PORT);
+});
